88371412397401e18dccdaa12cc8b115201bc76c
Chris Duflo <chris.duflo@gmail.com>
2016-02-03 15:25:38 -0800

    TV/Movie toggle added to search

--- MovieNerd.Web/js/app.js
+++ MovieNerd.Web/js/app.js
@@ -2,15 +2,35 @@
 
 angular.module('app').controller('indexController', function ($scope, $http) {
     $scope.searchTerm = '';
+    $scope.searchBy = "all";
+
+    $scope.changeSearch = function (x)
+        {
+        $scope.searchBy = x;
+        console.log($scope.searchBy);
+        }
 
     $scope.getMovies = function () {
-        $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm)
-        .success(function (data) {
-            $scope.movies = data.Search;
-            console.log(data);
-        })
-        .error(function (error) {
-            alert(error);
-        });
+        if ($scope.searchBy == "movie" || $scope.searchBy == "series") {
+            $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm + '&type=' + $scope.searchBy)
+            .success(function (data) {
+                $scope.movies = data.Search;
+                console.log(data);
+            })
+            .error(function (error) {
+                alert(error);
+            });
+        }
+        else {
+            $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm )
+            .success(function (data) {
+                $scope.movies = data.Search;
+                console.log(data);
+            })
+            .error(function (error) {
+                alert(error);
+            });
+        }
     };
+
 });
