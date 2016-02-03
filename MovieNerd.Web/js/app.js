5989235a74c724a57c76acc05637a8edbd81d6e1
Chris Duflo <chris.duflo@gmail.com>
2016-02-03 14:19:03 -0800

    core requirements complete

---
+++ MovieNerd.Web/js/app.js
@@ -0,0 +1,16 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('indexController', function ($scope, $http) {
+    $scope.searchTerm = '';
+
+    $scope.getMovies = function () {
+        $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm)
+        .success(function (data) {
+            $scope.movies = data.Search;
+            console.log(data);
+        })
+        .error(function (error) {
+            alert(error);
+        });
+    };
+});
